faa1f0e3b10c2cc6f756953babbe7e2692caf6a8
Tanner B <sonictanpat@gmail.com>
2025-01-15 13:31:59 -0600

    moving to playground

--- app.js
+++ app.js
@@ -1,8 +1,59 @@
-const getNotes = require('./notes.js')
 const chalk = require('chalk')
+const yargs  = require('yargs')
+const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
 
-console.log(chalk.red.bold.inverse('Error!'));
+// Customize yargs version
+yargs.version('1.1.0')
 
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        },
+    },
+    hander: function (argv) {
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List notes',
+    handler: function () {
+        console.log('Listing the note')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading the note')
+    }
+})
+
+// add, remove, read, list
+console.log(yargs.argv)
